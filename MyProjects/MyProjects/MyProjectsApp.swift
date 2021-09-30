e9c83ac0a621693b550000308f17d2f39f5b5a24
Lucas Nakano <lucas_nakano@hotmail.com>
2021-09-30 16:40:35 -0300

    Create Xcode Project

---
+++ MyProjects/MyProjects/MyProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectsApp.swift
+//  MyProjects
+//
+//  Created by Lucas Yoshio Nakano on 30/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
